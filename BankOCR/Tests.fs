7014d77ce6e06e451c6255060da653d271302cbd
Parajao <alex.digioia@gmail.com>
2015-02-20 19:19:06 +0100

    first commit

---
+++ BankOCR/Tests.fs
@@ -0,0 +1,15 @@
+﻿module Tests
+
+    open bankOcr
+    open NUnit.Framework
+    open FsCheck
+    open FsCheck.NUnit
+    open FsUnit 
+
+
+    [<Test>]
+    let ignore_me () = ()
+
+    [<Test>]
+    let assert_it_works () =
+        true |> should equal true 
